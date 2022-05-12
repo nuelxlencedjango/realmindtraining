575adaf35702b7cc37b50d904c54ae888fd99063
nuelxelence <nuelxelence@gmail.com>
2022-05-12 08:37:10 +0100

    Initial Django App

--- realmind/settings.py
+++ realmind/settings.py
@@ -10,12 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
+import sys
+import dj_database_url
+
+
+
 import cloudinary
-
-
 import django_heroku
+
+
 
 #import cloudinary_storage
 
@@ -27,13 +33,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%cy(6u9i(3kotxuc^&u5e^ec=nv127id5a^p)^187vrlgqh#kl'
+#SECRET_KEY = 'django-insecure-%cy(6u9i(3kotxuc^&u5e^ec=nv127id5a^p)^187vrlgqh#kl'
 
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ['www.realmindt.com','realmind.com','realmindt.herokuapp.com','127.0.0.1']
+#to decide later
+DEBUG = os.getenv("DEBUG", "False") == "True"
+#ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
+#ALLOWED_HOSTS = ['www.realmindt.com','realmind.com','realmindt.herokuapp.com','127.0.0.1']
+
+#DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
 
@@ -79,6 +93,24 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
+#if DEVELOPMENT_MODE is True:
+ #   DATABASES = {
+  #      "default": {
+   #         "ENGINE": "django.db.backends.sqlite3",
+    #        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+     #   }
+    #}
+#elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+ #   if os.getenv("DATABASE_URL", None) is None:
+  #      raise Exception("DATABASE_URL environment variable not defined")
+   # DATABASES = {
+    #    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    #}
+
+
 
 WSGI_APPLICATION = 'realmind.wsgi.application'
 
